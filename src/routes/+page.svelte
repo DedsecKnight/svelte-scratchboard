4e7178764a524ccc54f4070d958463b5ac7b5e66
DedsecKnight <mikenguyen7268@gmail.com>
2024-10-13 02:30:02 -0500

    feat: added light/dark theme toggle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
 	import ScratchboardTable from '$lib/components/index/scratchboard-table.svelte';
 	import { data as scratchboards } from '$lib/data/scratchboards';
+	import ThemeToggler from '@/components/ui/button/theme-toggler.svelte';
 </script>
 
 <div class="p-6">
-	<div>
-		<h1 class="text-2xl font-bold tracking-tight">My Svelte Scratchboard</h1>
-		<p class="text-muted-foreground">Here's a list of scratchboards you created!</p>
-		<div class="my-4">
-			<ScratchboardTable {scratchboards} />
+	<div class="flex items-center justify-between p-2">
+		<div>
+			<h1 class="text-2xl font-bold tracking-tight">My Svelte Scratchboard</h1>
+			<p class="text-muted-foreground">Here's a list of scratchboards you created!</p>
 		</div>
+		<ThemeToggler />
+	</div>
+	<div class="my-4">
+		<ScratchboardTable {scratchboards} />
 	</div>
 </div>
--- src/routes/scratchboards/+layout.svelte
+++ src/routes/scratchboards/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
 	import { Button } from '@/components/ui/button';
+	import ThemeToggler from '$lib/components/ui/button/theme-toggler.svelte';
 </script>
 
 <div class="flex flex-col gap-y-3 p-3">
-	<div class="w-fit">
+	<div class="mx-10 flex items-center justify-between">
 		<Button href="/" variant="outline">Back to home page</Button>
+		<ThemeToggler />
 	</div>
 	<slot></slot>
 </div>
